c847a9f7ceb8358e825284905921cbae96de9bfc
urigoren <ugoren@gmail.com>
2022-12-09 16:16:02 +0200

    better pypi desc

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ with open(__package__+"/__init__.py", 'r') as f:
         if line.startswith("__version__"):
             exec(line)
             break
+with open("README.md", 'r') as f:
+    long_description = f.read()
 setup(
     name=__package__,
     packages=[__package__],
@@ -15,14 +17,14 @@ setup(
         "pandas>=1.3.0",
         "scikit-learn>=0.19.0",
     ],
-    long_description="https://github.com/argmaxml/vecsim/blob/master/README.md",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     version=__version__,
-    description='',
+    description='Vector Similarity Search Engine',
     author='ArgmaxML',
     author_email='ugoren@argmax.ml',
     url='https://github.com/argmaxml/vecsim',
-    keywords=['vector-similarity','faiss','hnsw','redis','matching','ranking'],
+    keywords=['vector-similarity','faiss','hnsw','redis','matching','ranking','elasticsearch','search','embedding'],
     classifiers=[],
     extras_require = {
         'faiss': ['faiss-cpu>=1.7.1'],
