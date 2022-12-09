9af22d9a5dcba4234d926c2ad08091a97114d6ca
urigoren <ugoren@gmail.com>
2022-12-09 16:00:58 +0200

    bugfix

--- vecsim/__init__.py
+++ vecsim/__init__.py
@@ -1,3 +1,3 @@
-__version__="0.0.3"
-from .similarity_helpers import RedisIndex,SciKitIndex,FaissIndex
+__version__="0.0.31"
+from .similarity_helpers import RedisIndex,SciKitIndex,FaissIndex,Elasticsearch
 from .similarity_helpers import available_engines
